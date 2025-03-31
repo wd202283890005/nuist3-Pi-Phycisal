b78a6e3670e3105e4186263fce63ede590951b3d
wwdd <202283890005@nuist.edu.cn>
2025-03-30 18:57:26 -0900

    second commit with comments

--- LED.py
+++ LED.py
@@ -1,13 +1,21 @@
-import RPi.GPIO as GPIO
-import time
+# Import necessary libraries
+import RPi.GPIO as GPIO  # Library for GPIO control
+import time              # Library for time-related functions
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(18, GPIO.OUT)
+# Set up GPIO mode and disable warnings
+GPIO.setmode(GPIO.BCM)   # Use Broadcom SOC channel numbering (BCM)
+GPIO.setwarnings(False)  # Disable runtime warning messages
 
-print("LED turn on")
-GPIO.output(18, GPIO.HIGH)  
-time.sleep(1)               
+# Configure GPIO pin 18 as output
+GPIO.setup(18, GPIO.OUT) # Set GPIO18 as output pin
 
-print("LED turn off")
-GPIO.output(18, GPIO.LOW)   
+# Turn LED on
+print("LED ON")                   # Print status to console
+GPIO.output(18, GPIO.HIGH)        # Set pin to HIGH (3.3V) to turn LED on
+time.sleep(1)                     # Keep LED on for 1 second
+
+# Turn LED off
+print("LED OFF")                  # Print status to console
+GPIO.output(18, GPIO.LOW)         # Set pin to LOW (0V) to turn LED off
+
+
